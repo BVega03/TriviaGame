4dbd32dc3bb8db4ca3c78e24865df4dab7b6640f
Benny V <bengv2303@gmail.com>
2018-11-04 23:42:09 -0700

    added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -42,6 +42,33 @@ function check() {
 
 }
 
+var countDownDate = new Time("00:10").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    var now = new Time().getTime();
+
+    // Find the distance between now and the count down date
+    // var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    // document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+    //     minutes + "m " + seconds + "s ";
+    document.getElementById("timer").innerHTML = seconds + "s ";
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "Times Up!";
+    }
+}, 1000);
+
 // Why will this only work if I insert it in the html "head" with a  script in javascript?
 // var score;
 // score = 0;
